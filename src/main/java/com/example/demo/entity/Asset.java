6124c8f9e1cbd809f2c8f068580d32d30557b0f4
bala-amypo <bala@amypo.in>
2025-12-19 05:50:40 +0000

    User commit

--- src/main/java/com/example/demo/entity/Asset.java
+++ src/main/java/com/example/demo/entity/Asset.java
@@ -3,81 +3,85 @@ package com.example.demo.entity;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Asset {
-    @
-private Long id;
-private String assetTag;
-public Long getId() {
-    return id;
-}
-public void setId(Long id) {
-    this.id = id;
-}
-public String getAssetTag() {
-    return assetTag;
-}
-public void setAssetTag(String assetTag) {
-    this.assetTag = assetTag;
-}
-public String getAssetType() {
-    return assetType;
-}
-public void setAssetType(String assetType) {
-    this.assetType = assetType;
-}
-public String getModel() {
-    return model;
-}
-public void setModel(String model) {
-    this.model = model;
-}
-public LocalDate getPurchaseDate() {
-    return purchaseDate;
-}
-public void setPurchaseDate(LocalDate purchaseDate) {
-    this.purchaseDate = purchaseDate;
-}
-public String getStatus() {
-    return status;
-}
-public void setStatus(String status) {
-    this.status = status;
-}
-public User getCurrentHolder() {
-    return currentHolder;
-}
-public void setCurrentHolder(User currentHolder) {
-    this.currentHolder = currentHolder;
-}
-public LocalDateTime getCreatedAt() {
-    return createdAt;
-}
-public void setCreatedAt(LocalDateTime createdAt) {
-    this.createdAt = createdAt;
-}
-private String assetType;
-public Asset(Long id, String assetType, String model, LocalDate purchaseDate, String status, User currentHolder,
-        LocalDateTime createdAt) {
-    this.id = id;
-    this.assetType = assetType;
-    this.model = model;
-    this.purchaseDate = purchaseDate;
-    this.status = status;
-    this.currentHolder = currentHolder;
-    this.createdAt = createdAt;
-}
-private String model;
-private LocalDate purchaseDate;
-private String status;
-private User currentHolder;
-private LocalDateTime createdAt;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getAssetTag() {
+        return assetTag;
+    }
+    public void setAssetTag(String assetTag) {
+        this.assetTag = assetTag;
+    }
+    public String getAssetType() {
+        return assetType;
+    }
+    public void setAssetType(String assetType) {
+        this.assetType = assetType;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public User getCurrentHolder() {
+        return currentHolder;
+    }
+    public void setCurrentHolder(User currentHolder) {
+        this.currentHolder = currentHolder;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    @Column(unique = true)
+    private String assetTag;
+    private String assetType;
+    public Asset(String assetTag, String assetType, String model, LocalDate purchaseDate, String status,
+            User currentHolder, LocalDateTime createdAt) {
+        this.assetTag = assetTag;
+        this.assetType = assetType;
+        this.model = model;
+        this.purchaseDate = purchaseDate;
+        this.status = status;
+        this.currentHolder = currentHolder;
+        this.createdAt = createdAt;
+    }
+    private String model;
+    private LocalDate purchaseDate;
+    private String status;
+    private User currentHolder;
+    private LocalDateTime createdAt;
 
-public Asset(){
-
-}
-
+    public Asset(){
+        
+    }
 }
