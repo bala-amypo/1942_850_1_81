d098354705e072799f30d293cef511994d164790
bala-amypo <bala@amypo.in>
2025-12-23 03:38:42 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DisposalRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DisposalRecordServiceImpl.java
@@ -2,9 +2,11 @@ package com.example.demo.service.impl;
 
 import com.example.demo.entity.Asset;
 import com.example.demo.entity.DisposalRecord;
+import com.example.demo.entity.User;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.AssetRepository;
 import com.example.demo.repository.DisposalRecordRepository;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.DisposalRecordService;
 import org.springframework.stereotype.Service;
 
@@ -15,21 +17,34 @@ public class DisposalRecordServiceImpl implements DisposalRecordService {
 
     private final DisposalRecordRepository disposalRepo;
     private final AssetRepository assetRepo;
+    private final UserRepository userRepo;  
 
     public DisposalRecordServiceImpl(
             DisposalRecordRepository disposalRepo,
-            AssetRepository assetRepo) {
+            AssetRepository assetRepo,
+            UserRepository userRepo) {       
         this.disposalRepo = disposalRepo;
         this.assetRepo = assetRepo;
+        this.userRepo = userRepo;            
     }
 
     @Override
     public DisposalRecord createDisposal(Long assetId, DisposalRecord disposal) {
 
+       
         Asset asset = assetRepo.findById(assetId)
                 .orElseThrow(() -> new ResourceNotFoundException("Asset not found"));
 
+    
+        User approver = userRepo.findById(
+                disposal.getApprovedBy().getId()
+        ).orElseThrow(() -> new ResourceNotFoundException("Approver not found"));
+
+    
         disposal.setAsset(asset);
+        disposal.setApprovedBy(approver);
+
+       
         return disposalRepo.save(disposal);
     }
 
