cfb80645bdab4d5d1cadb15e6f2787302351606d
bala-amypo <bala@amypo.in>
2025-12-26 15:59:37 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,12 +2,8 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
 
 @Configuration
@@ -15,21 +11,10 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-
-        SecurityScheme bearerAuth = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER)
-                .name("Authorization");
-
         return new OpenAPI()
+                // You need to change the port as per your server
                 .servers(List.of(
                         new Server().url("https://9243.408procr.amypo.ai/")
-                ))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth", bearerAuth)
-                )
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
-    }
+                ));
+        }
 }
